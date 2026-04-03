919569d5ca89422495b0a9d594c1ba140211249b
Matt <verniermatthieu@orange.fr>
2026-04-03 18:04:33 +0200

    feat: optimize Sentry config for production

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,11 +6,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1,
+  environment: process.env.NODE_ENV ?? "development",
+  release: process.env.VERCEL_GIT_COMMIT_SHA,
 });
